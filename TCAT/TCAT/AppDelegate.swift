1aae476237f891b686fa15540cd6929b04f5fa4f
AAAstorga <astorgaaustin@gmail.com>
2017-03-01 17:58:09 -0500

    more merge conflicts resolved in pbxproj

--- TCAT/TCAT/AppDelegate.swift
+++ TCAT/TCAT/AppDelegate.swift
@@ -13,11 +13,11 @@ import SwiftyJSON
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     let userDefaults = UserDefaults.standard
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -26,47 +26,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         GMSServices.provideAPIKey(json["google-maps"].stringValue)
         GMSPlacesClient.provideAPIKey(json["google-places"].stringValue)
         
+        //Set Up Navigation Controller
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
+        statusBar.backgroundColor = .white
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let nav1 = UINavigationController()
+        nav1.extendedLayoutIncludesOpaqueBars = true
         nav1.navigationBar.isTranslucent = true
         nav1.navigationBar.tintColor = .white
+        nav1.navigationBar.backgroundColor = .white
         let mainView = HomeViewController()
         nav1.viewControllers = [mainView]
         self.window!.rootViewController = nav1
         self.window?.makeKeyAndVisible()
         
-        //check if userdefaults for recentLocations exsits, if not make one
-       // if userDefaults.value(forKey: "recentLocations") == nil {
-       //     userDefaults.set(, forKey: "recentLocations")
-       // }
-    
-        
-        
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
