7857790ab32d4145defa1165a114dedd209d7578
Monica Ong <myo3@cornell.edu>
2017-03-05 14:39:25 -0500

    Position items on cell draft. Added 1 more method to Time class

--- TCAT/TCAT/Cells/RouteTableViewCell.swift
+++ TCAT/TCAT/Cells/RouteTableViewCell.swift
@@ -20,7 +20,6 @@ extension UIColor {
 
 }
 
-//N2SELF: got to change -1 to mean pins
 class RouteTableViewCell: UITableViewCell {
 
     //Data
@@ -28,7 +27,7 @@ class RouteTableViewCell: UITableViewCell {
     var arrivalTime: Date?
     var stops: [String] = [] //mainStops
     var stopNums: [Int] = [] //mainStopsNum, 0 for pins
-    var distance: Double? //of first stop
+    var distance: Double = 0 //of first stop
     
     //View
     var travelTimeLabel: UILabel = UILabel()
@@ -37,7 +36,10 @@ class RouteTableViewCell: UITableViewCell {
     var stopNumButtons: [UIButton] = []
     var arrows: [UIImageView] = []
     var distanceLabel: UILabel = UILabel()
-    var header: UIView = UIView()
+    
+    
+    //Spacing
+    let space: CGFloat = 8.0
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -52,34 +54,8 @@ class RouteTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        //Set font, font size, color, & frame
-        travelTimeLabel.frame = CGRect(x: 0, y: 0, width: 135, height: 20)
-        travelTimeLabel.font = UIFont(name: "SFUIText-Regular", size: 14.0)
-        travelTimeLabel.textColor = .travelTimeColor
+
         
-        departTimeLabel.frame = CGRect(x: 0, y: 0, width: 135, height: 20)
-        departTimeLabel.font = UIFont(name: "SFUIText-Regular", size: 14.0)
-        departTimeLabel.textColor = .departTimeColor
-        
-        distanceLabel.frame = CGRect(x: 0, y: 0, width: 90, height: 20)
-        distanceLabel.font = UIFont(name: "SFUIText-Regular", size: 12.0)
-        distanceLabel.textColor = .distanceLabelColor
-        
-        //Set up header size
-        header.frame = CGRect(x: 0, y: 0, width: self.contentView.frame.width, height: 25)
-        
-        //Set up & activate constraints for header
-        self.addSubview(header)
-        
-        let headerTopConstraint = NSLayoutConstraint(item: header, attribute: .top, relatedBy: .equal, toItem: self.contentView, attribute: .top, multiplier: 1.0, constant: 0.0)
-        let headerRightConstraint = NSLayoutConstraint(item: header, attribute: .trailing, relatedBy: .equal, toItem: self.contentView, attribute: .trailing, multiplier: 1.0, constant: 0.0)
-        let headerLeftConstraint = NSLayoutConstraint(item: header, attribute: .leading, relatedBy: .equal, toItem: self.contentView, attribute: .leading, multiplier: 1.0, constant: 0.0)
-        
-        headerTopConstraint.identifier = "headerTopConstraint"
-        headerRightConstraint.identifier = "headerRightConstraint"
-        headerLeftConstraint.identifier = "headerLeftConstraint"
-        
-        NSLayoutConstraint.activate([headerTopConstraint, headerRightConstraint, headerLeftConstraint])
     }
     
     /*Call this function after pass all data to cell 
@@ -87,60 +63,85 @@ class RouteTableViewCell: UITableViewCell {
      */
     func setData(){
         
-        //Set up time label text, frame & (activate) constraints
+        //Set up time label text, frame
+        travelTimeLabel.frame = CGRect(x: space*1.5, y: space, width: 135, height: 20)
+        travelTimeLabel.font = UIFont(name: "SFUIText-Regular", size: 14.0)
+        travelTimeLabel.textColor = .travelTimeColor
         travelTimeLabel.text = "\(Time.string(from: departureTime!)) - \(Time.string(from: arrivalTime!))"
         travelTimeLabel.sizeToFit()
         
-        header.addSubview(travelTimeLabel)
-        
-        let timeTopConstraint = NSLayoutConstraint(item: travelTimeLabel, attribute: .top, relatedBy: .equal, toItem: header, attribute: .top, multiplier: 1.0, constant: 8.0)
-        let timeLeftConstraint = NSLayoutConstraint(item: travelTimeLabel, attribute: .leading, relatedBy: .equal, toItem: header, attribute: .leading, multiplier: 1.0, constant: 8.0)
-        let timeBottomConstraint = NSLayoutConstraint(item: travelTimeLabel, attribute: .bottom, relatedBy: .equal, toItem: header, attribute: .bottom, multiplier: 1.0, constant: 0.0)
-        
-        timeTopConstraint.identifier = "timeTopConstraint"
-        timeLeftConstraint.identifier = "timeLeftConstraint"
-        timeBottomConstraint.identifier = "timeBottomConstraint"
-        
-        NSLayoutConstraint.activate([timeTopConstraint, timeLeftConstraint, timeBottomConstraint])
+        contentView.addSubview(travelTimeLabel)
         
         //Generate depart label text
         //Generate time string based on time until departure
-        let timeUntilDeparture = Time.dateComponents(from: Date(), to: departureTime!)
+        //N2SELF: got to make sure time calculations work w/ better times (it's not working b/c times are off)
+        let timeUntilDeparture = Time.dateComponents(from: departureTime!, to: Date())
         var timeStr = ""
-        if(timeUntilDeparture.day! > 1){
-            timeStr += "\(timeUntilDeparture.day) days"
-        }else if(timeUntilDeparture.day! > 0){
-            timeStr += "\(timeUntilDeparture.day) day"
+        if(timeUntilDeparture.day! > 0){
+            timeStr += "\(timeUntilDeparture.day!) d "
         }
-        if(timeUntilDeparture.hour! > 1){
-            timeStr += "\(timeUntilDeparture.hour) hours"
-        }else if(timeUntilDeparture.hour! > 0){
-            timeStr += "\(timeUntilDeparture.hour) hour"
+        if(timeUntilDeparture.hour! > 0){
+            timeStr += "\(timeUntilDeparture.hour!) hr "
         }
         if(timeUntilDeparture.minute! > 0 ){
-            timeStr += "\(timeUntilDeparture.minute) min"
+            timeStr += "\(timeUntilDeparture.minute!) min"
         }
         
-        //Set up depart lable text, frame, & (activate) constraints
+        print("Time string: \(timeStr)")
+        //Set up depart lable text, &  frame
+        departTimeLabel.frame = CGRect(x: 0, y: travelTimeLabel.frame.minY, width: 135, height: 20)
+        departTimeLabel.font = UIFont(name: "SFUIText-Regular", size: 14.0)
+        departTimeLabel.textColor = .departTimeColor
         departTimeLabel.text = "Departs in \(timeStr)"
         departTimeLabel.sizeToFit()
+        departTimeLabel.center.x = contentView.frame.width - space - (departTimeLabel.frame.width/2)
         
-        header.addSubview(departTimeLabel)
+        contentView.addSubview(departTimeLabel)
         
-        let departTopConstraint = NSLayoutConstraint(item: departTimeLabel, attribute: .top, relatedBy: .equal, toItem: travelTimeLabel, attribute: .top, multiplier: 1.0, constant: 0.0)
-        let departRightConstraint = NSLayoutConstraint(item: header, attribute: .trailing, relatedBy: .equal, toItem: departTimeLabel, attribute: .trailing, multiplier: 1.0, constant: 8)
-        let departBottomConstraint = NSLayoutConstraint(item: departTimeLabel, attribute: .bottom, relatedBy: .equal, toItem: travelTimeLabel, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+        //Create stopNumButtons & format
+        for i in 0...(stopNums.count-1){
+            stopNumButtons.append(UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30)))
+            if(stopNums[i] == -1){ //pin
+                stopNumButtons[i].setImage(UIImage(named: "pin"), for: .normal)
+                stopNumButtons[i].contentMode = .scaleAspectFit
+                stopNumButtons[i].tintColor = .pinColor                
+            }else{
+                stopNumButtons[i].setTitle("\(stopNums[i])", for: .normal)
+                stopNumButtons[i].titleLabel?.font = UIFont(name: "SFUIDisplay-Regular", size: 13.0)
+                stopNumButtons[i].setTitleColor(.white, for: .normal)
+                stopNumButtons[i].backgroundColor = .stopNumColor1
+                stopNumButtons[i].layer.cornerRadius = stopNumButtons[i].frame.width/2
+                stopNumButtons[i].layer.masksToBounds = true //N2SELF: Do i need this?
+            }
+            stopNumButtons[i].sizeToFit()
+            self.addSubview(stopNumButtons[i])
+        }
         
-        departTopConstraint.identifier = "departTopConstraint"
-        departRightConstraint.identifier = "departRightConstraint"
-        departBottomConstraint.identifier = "departBottomConstraint"
+        //Create arrows (one less arrows than stops buttons)
+        for i in 0...stopNumButtons.count-2{
+            arrows.append(UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: 6)))
+            arrows[i].image = UIImage(named: "arrow")
+            arrows[i].contentMode = .scaleAspectFit
+            self.addSubview(arrows[i])
+        }
         
-        NSLayoutConstraint.activate([departTopConstraint,departRightConstraint,departBottomConstraint])
+        //Positon buttons & arrows positions
+        for i in 0...(stopNumButtons.count-1){
+            if(i == 0){ //set position of first pins
+                stopNumButtons[i].center.x = space + (stopNumButtons[i].frame.width/2)
+                stopNumButtons[i].center.y = travelTimeLabel.frame.maxY + space + (stopNumButtons[i].frame.height/2)
+            }else{//set position of other pins & arrows
+                arrows[i-1].center.x = stopNumButtons[i-1].center.x
+                stopNumButtons[i].center.x = stopNumButtons[i-1].center.x
+
+                arrows[i-1].center.y = stopNumButtons[i-1].frame.maxY + space + (arrows[i-1].frame.height/2)
+                stopNumButtons[i].center.y = arrows[i-1].frame.maxY + space + (stopNumButtons[i].frame.height/2)
+            }
+        }
         
-        
-        //Set up stops text & frame
+        //Create stops text
         for i in 0...((stops.count)-1) {
-            stopLabels[i] = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 20))
+            stopLabels.append(UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 20)))
             stopLabels[i].text = stops[i]
             stopLabels[i].font = UIFont(name: "SFUIText-Regular", size: 14.0)
             stopLabels[i].textColor = .stopLabelColor
@@ -149,91 +150,21 @@ class RouteTableViewCell: UITableViewCell {
             self.addSubview(stopLabels[i])
         }
         
-        //Set up stopNumButtons & format
-        for i in 0...(stopNums.count-1){
-            stopNumButtons[i] = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-            if(stopNums[i] == 0){ //pin
-                stopNumButtons[i].setImage(UIImage(named: "pin"), for: .normal)
-                stopNumButtons[i].contentMode = .scaleAspectFit
-                stopNumButtons[i].tintColor = .pinColor
-                
-            }else{
-                stopNumButtons[i].setTitle("\(stopNums[i])", for: .normal)
-                stopNumButtons[i].titleLabel?.font = UIFont(name: "SFUIDisplay-Regular", size: 13.0)
-                stopNumButtons[i].setTitleColor(.white, for: .normal)
-                stopNumButtons[i].backgroundColor = .stopNumColor1
-                stopNumButtons[i].layer.cornerRadius = stopNumButtons[i].frame.width/2
-                stopNumButtons[i].layer.masksToBounds = true //Do i need this?
-            }
-            stopNumButtons[i].sizeToFit()
-            self.addSubview(stopNumButtons[i])
+        //Position stops
+        for i in 0...(stopLabels.count-1){
+            stopLabels[i].center.x = stopNumButtons[i].frame.maxX + space + (stopLabels[i].frame.width/2)
+            stopLabels[i].center.y = stopNumButtons[i].center.y
         }
         
-        //Create arrows (one less arrows than stops buttons)
-        for i in 0...stopNumButtons.count-2{
-            arrows[i] = UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: 6))
-            arrows[i].image = UIImage(named: "arrow")
-            arrows[i].contentMode = .scaleAspectFit
-            self.addSubview(arrows[i])
-        }
-        
-        //Set (& activate) first stop label, num, and distance label
-        let stopNum1LeftConstraint = NSLayoutConstraint(item: stopNums[0], attribute: .leading, relatedBy: .equal, toItem: self.contentView, attribute: .leading, multiplier: 1.0, constant: 8.0)
-        let stopNum1TopConstraint = NSLayoutConstraint(item: stopNums[0], attribute: .top, relatedBy: .equal, toItem: header, attribute: .bottom, multiplier: 1.0, constant: 8.0)
-        let stopLabel1LeftConstraint = NSLayoutConstraint(item: stopLabels[0], attribute: .leading, relatedBy: .equal, toItem: stopNums[0], attribute: .trailing, multiplier: 1.0, constant: 8.0)
-        let stopLabel1CenterConstraint = NSLayoutConstraint(item: stopLabels[0], attribute: .centerY, relatedBy: .equal, toItem: stopNums[0], attribute: .centerY, multiplier: 1.0, constant: 0.0)
-        let arrow1TopConstraint = NSLayoutConstraint(item: arrows[0], attribute: .top, relatedBy: .equal, toItem: stopNums[0], attribute: .bottom, multiplier: 1.0, constant: 8.0)
-        let arrow1CenterConstraint = NSLayoutConstraint(item: arrows[0], attribute: .centerX, relatedBy: .equal, toItem: stopNums[0], attribute: .centerX, multiplier: 1.0, constant: 0.0)
-        
-        stopNum1LeftConstraint.identifier = "stopNum1LeftConstraint"
-        stopNum1TopConstraint.identifier = "stopNum1TopConstraint"
-        stopLabel1LeftConstraint.identifier = "stopLabel1LeftConstraint"
-        stopLabel1CenterConstraint.identifier = "stopLabel1CenterConstraint"
-        arrow1TopConstraint.identifier = "arrow1TopConstraint"
-        arrow1CenterConstraint.identifier = "arrow1CenterConstraint"
-        
-        NSLayoutConstraint.activate([stopNum1LeftConstraint,stopNum1TopConstraint,stopLabel1LeftConstraint,stopLabel1CenterConstraint,arrow1TopConstraint,arrow1CenterConstraint])
-        
-        //Set up & activate constraints for all stop nums & stop labels
-        for i in 1...(stopLabels.count - 1) {
-            let stopNumTopConstraint = NSLayoutConstraint(item: stopNums[i], attribute: .top, relatedBy: .equal, toItem: arrows[i-1], attribute: .bottom, multiplier: 1.0, constant: 8.0)
-            let stopNumCenterXConstraint = NSLayoutConstraint(item: stopNums[i], attribute: .centerX, relatedBy: .equal, toItem: stopNums[i-1], attribute: .centerX, multiplier: 1.0, constant: 0.0)
-            let stopLabelLeftConstraint = NSLayoutConstraint(item: stopLabels[i], attribute: .leading, relatedBy: .equal, toItem: stopLabels[i-1], attribute: .leading, multiplier: 1.0, constant: 0.0)
-            let stopLabelCenterYConstraint = NSLayoutConstraint(item: stopLabels[i], attribute: .centerX, relatedBy: .equal, toItem: stopNums[i], attribute: .centerX, multiplier: 1.0, constant: 0.0)
-            
-            stopNumTopConstraint.identifier = "stopNum\(i)TopConstraint"
-            stopNumCenterXConstraint.identifier = "stopNum\(i)CenterXConstraint"
-            stopLabelLeftConstraint.identifier = "stopLabel\(i)LeftConstraint"
-            stopLabelCenterYConstraint.identifier = "stopLabel\(i)CenterYConstraint"
-            
-            NSLayoutConstraint.activate([stopNumTopConstraint, stopNumCenterXConstraint, stopLabelLeftConstraint, stopLabelCenterYConstraint])
-            
-        }
-        
-        //Set up & activite constraints for all arrows
-        for i in 1...(arrows.count-1){
-            let arrowTopConstraint = NSLayoutConstraint(item: arrows[i], attribute: .bottom, relatedBy: .equal, toItem: stopNums[i], attribute: .centerX, multiplier: 1.0, constant: 0.0)
-            let arrowCenterXConstraint = NSLayoutConstraint(item: arrows[i], attribute: .centerX, relatedBy: .equal, toItem: stopNums[0], attribute: .centerX, multiplier: 1.0, constant: 0.0)
-            
-            arrowTopConstraint.identifier = "arrow\(i)TopConstraint"
-            arrowCenterXConstraint.identifier = "arrow\(i)CenterXConstraint"
-            
-            NSLayoutConstraint.activate([arrowTopConstraint,arrowCenterXConstraint])
-        }
-        
-        //Set up & activate constrains for distance label
+        //Set up distance label
+        distanceLabel.frame = CGRect(x: stopLabels[0].frame.maxX + space, y: stopLabels[0].frame.minY, width: 90, height: 20)
+        distanceLabel.font = UIFont(name: "SFUIText-Regular", size: 12.0)
+        distanceLabel.textColor = .distanceLabelColor
         distanceLabel.text = "\(distance) mi away"
         distanceLabel.sizeToFit()
         
         self.addSubview(distanceLabel)
         
-        let distLabelLeftConstraint = NSLayoutConstraint(item: distanceLabel, attribute: .leading, relatedBy: .equal, toItem: stopLabels[0], attribute: .trailing, multiplier: 1.0, constant: 8.0)
-        let distLabelCenterYConstraint = NSLayoutConstraint(item: distanceLabel, attribute: .centerY, relatedBy: .equal, toItem: stopNums[0], attribute: .centerY, multiplier: 1.0, constant: 0.0)
-        
-        distLabelLeftConstraint.identifier = "distanceLabelLeftConstraint"
-        distLabelCenterYConstraint.identifier = "distanceLabelCenterYConstraint"
-        
-        NSLayoutConstraint.activate([distLabelLeftConstraint, distLabelCenterYConstraint])
         
     }
     
--- TCAT/TCAT/Utilities/Time.swift
+++ TCAT/TCAT/Utilities/Time.swift
@@ -24,13 +24,27 @@ class Time{
         return Calendar.current.dateComponents([.hour, .minute, .day], from: startTime, to: endTime)
     }
     
+    /*Calculates dateComponenets for a single date
+     */
+    static func dateComponents(from date: Date) -> DateComponents{
+        return Calendar.current.dateComponents([.year,.month,.day, .hour, .minute], from: date)
+    }
+    
     /*Takes time string formatted in "h:mm a" and returns today's date with that time
      * Throws error if string not formatted in "h:mm a"
      */
     static func date(from string: String) -> Date{
+        //Get date
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "h:mm a"
-        return dateFormatter.date(from: string)!
+        let date = dateFormatter.date(from: string)
+        var dateComponents = Time.dateComponents(from: date!)
+        //Modify date to have today's day, month & year
+        var todaysDateComponents = Time.dateComponents(from: Date())
+        dateComponents.year = todaysDateComponents.year
+        dateComponents.month = todaysDateComponents.month
+        dateComponents.day = todaysDateComponents.day
+        return Calendar.current.date(from: dateComponents)!
         
     }
 }
