91b1e4d5b2881249861809dec61c88366926d6c2
Annie Cheng <ac962@cornell.edu>
2017-03-05 15:47:38 -0500

    Add Color Extensions file

---
+++ TCAT/TCAT/Utilities/UIColor+Shared.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+Shared.swift
+//  TCAT
+//
+//  Created by Annie Cheng on 3/5/17.
+//  Copyright © 2017 cuappdev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    @nonobjc static let departTimeColor = UIColor(red: 146/255, green: 146/255, blue: 146/255, alpha: 1.0)
+    @nonobjc static let travelTimeColor = UIColor(red: 100/255, green: 100/255, blue: 100/255, alpha: 1.0)
+    @nonobjc static let stopLabelColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1.0)
+    @nonobjc static let stopNumColor1 = UIColor(red: 243/255, green: 156/255, blue: 18/255, alpha: 1.0)
+    @nonobjc static let stopNumColor2 = UIColor(red: 255/255, green: 97/255, blue: 116/255, alpha: 1.0)
+    @nonobjc static let distanceLabelColor = UIColor(red: 187/255, green: 187/255, blue: 187/255, alpha: 1.0)
+    @nonobjc static let pinColor = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
+    
+}
