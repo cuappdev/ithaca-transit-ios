4c3aee1af5f94c196dcc1a1892c97351c5071d03
Monica Ong <myo3@cornell.edu>
2017-03-05 15:05:00 -0500

    Working on laying out cells

--- TCAT/TCAT/Cells/RouteTableViewCell.swift
+++ TCAT/TCAT/Cells/RouteTableViewCell.swift
@@ -37,7 +37,6 @@ class RouteTableViewCell: UITableViewCell {
     var arrows: [UIImageView] = []
     var distanceLabel: UILabel = UILabel()
     
-    
     //Spacing
     let space: CGFloat = 8.0
     
@@ -54,27 +53,29 @@ class RouteTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-
         
+        //Set up time label text, frame
+        travelTimeLabel.frame = CGRect(x: space*1.5, y: space, width: 135, height: 20)
+        travelTimeLabel.font = UIFont(name: "SFUIText-Regular", size: 14.0)
+        travelTimeLabel.textColor = .travelTimeColor
+        contentView.addSubview(travelTimeLabel)
+        
+        //Set up depart label text, &  frame
+        departTimeLabel.frame = CGRect(x: 0, y: travelTimeLabel.frame.minY, width: 135, height: 20)
+        departTimeLabel.font = UIFont(name: "SFUIText-Regular", size: 14.0)
+        departTimeLabel.textColor = .departTimeColor
+        contentView.addSubview(departTimeLabel)
     }
     
     /*Call this function after pass all data to cell 
       * in order to set cell with this data
      */
     func setData(){
-        
-        //Set up time label text, frame
-        travelTimeLabel.frame = CGRect(x: space*1.5, y: space, width: 135, height: 20)
-        travelTimeLabel.font = UIFont(name: "SFUIText-Regular", size: 14.0)
-        travelTimeLabel.textColor = .travelTimeColor
+        //Input travel time
         travelTimeLabel.text = "\(Time.string(from: departureTime!)) - \(Time.string(from: arrivalTime!))"
         travelTimeLabel.sizeToFit()
         
-        contentView.addSubview(travelTimeLabel)
-        
-        //Generate depart label text
         //Generate time string based on time until departure
-        //N2SELF: got to make sure time calculations work w/ better times (it's not working b/c times are off)
         let timeUntilDeparture = Time.dateComponents(from: departureTime!, to: Date())
         var timeStr = ""
         if(timeUntilDeparture.day! > 0){
@@ -87,16 +88,9 @@ class RouteTableViewCell: UITableViewCell {
             timeStr += "\(timeUntilDeparture.minute!) min"
         }
         
-        print("Time string: \(timeStr)")
-        //Set up depart lable text, &  frame
-        departTimeLabel.frame = CGRect(x: 0, y: travelTimeLabel.frame.minY, width: 135, height: 20)
-        departTimeLabel.font = UIFont(name: "SFUIText-Regular", size: 14.0)
-        departTimeLabel.textColor = .departTimeColor
+        //Input depart time
         departTimeLabel.text = "Departs in \(timeStr)"
         departTimeLabel.sizeToFit()
-        departTimeLabel.center.x = contentView.frame.width - space - (departTimeLabel.frame.width/2)
-        
-        contentView.addSubview(departTimeLabel)
         
         //Create stopNumButtons & format
         for i in 0...(stopNums.count-1){
@@ -104,7 +98,7 @@ class RouteTableViewCell: UITableViewCell {
             if(stopNums[i] == -1){ //pin
                 stopNumButtons[i].setImage(UIImage(named: "pin"), for: .normal)
                 stopNumButtons[i].contentMode = .scaleAspectFit
-                stopNumButtons[i].tintColor = .pinColor                
+                stopNumButtons[i].tintColor = .pinColor
             }else{
                 stopNumButtons[i].setTitle("\(stopNums[i])", for: .normal)
                 stopNumButtons[i].titleLabel?.font = UIFont(name: "SFUIDisplay-Regular", size: 13.0)
@@ -114,7 +108,6 @@ class RouteTableViewCell: UITableViewCell {
                 stopNumButtons[i].layer.masksToBounds = true //N2SELF: Do i need this?
             }
             stopNumButtons[i].sizeToFit()
-            self.addSubview(stopNumButtons[i])
         }
         
         //Create arrows (one less arrows than stops buttons)
@@ -122,21 +115,6 @@ class RouteTableViewCell: UITableViewCell {
             arrows.append(UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: 6)))
             arrows[i].image = UIImage(named: "arrow")
             arrows[i].contentMode = .scaleAspectFit
-            self.addSubview(arrows[i])
-        }
-        
-        //Positon buttons & arrows positions
-        for i in 0...(stopNumButtons.count-1){
-            if(i == 0){ //set position of first pins
-                stopNumButtons[i].center.x = space + (stopNumButtons[i].frame.width/2)
-                stopNumButtons[i].center.y = travelTimeLabel.frame.maxY + space + (stopNumButtons[i].frame.height/2)
-            }else{//set position of other pins & arrows
-                arrows[i-1].center.x = stopNumButtons[i-1].center.x
-                stopNumButtons[i].center.x = stopNumButtons[i-1].center.x
-
-                arrows[i-1].center.y = stopNumButtons[i-1].frame.maxY + space + (arrows[i-1].frame.height/2)
-                stopNumButtons[i].center.y = arrows[i-1].frame.maxY + space + (stopNumButtons[i].frame.height/2)
-            }
         }
         
         //Create stops text
@@ -146,8 +124,54 @@ class RouteTableViewCell: UITableViewCell {
             stopLabels[i].font = UIFont(name: "SFUIText-Regular", size: 14.0)
             stopLabels[i].textColor = .stopLabelColor
             stopLabels[i].sizeToFit()
-            
-            self.addSubview(stopLabels[i])
+        }
+        
+        //Set up distance label
+        distanceLabel.font = UIFont(name: "SFUIText-Regular", size: 12.0)
+        distanceLabel.textColor = .distanceLabelColor
+        distanceLabel.text = "\(distance) mi away"
+        distanceLabel.sizeToFit()
+    }
+    
+    func addSubviewsOnce(){
+        for i in 0...(stopNumButtons.count-1){
+            contentView.addSubview(stopNumButtons[i])
+        }
+        
+        for i in 0...(arrows.count-1){
+            contentView.addSubview(arrows[i])
+        }
+        
+        for i in 0...(stopLabels.count-1){
+            contentView.addSubview(stopLabels[i])
+        }
+        
+        contentView.addSubview(distanceLabel)
+
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        //Position depart time label
+         departTimeLabel.center.x = contentView.frame.width - space - (departTimeLabel.frame.width/2)
+        
+        //Positon buttons & arrows positions
+        for i in 0...(stopNumButtons.count-1){
+            if(i == 0){ //set position of first pins
+                stopNumButtons[i].center.x = space + (stopNumButtons[i].frame.width/2)
+                stopNumButtons[i].center.y = travelTimeLabel.frame.maxY + space + (stopNumButtons[i].frame.height/2)
+            }else{//set position of other pins & arrows
+                arrows[i-1].center.x = stopNumButtons[i-1].center.x
+                stopNumButtons[i].center.x = stopNumButtons[i-1].center.x
+                
+                arrows[i-1].center.y = stopNumButtons[i-1].frame.maxY + space + (arrows[i-1].frame.height/2)
+                stopNumButtons[i].center.y = arrows[i-1].frame.maxY + space + (stopNumButtons[i].frame.height/2)
+                print("arrows \(i-1): \(arrows[i-1].frame.minY)")
+            }
         }
         
         //Position stops
@@ -158,22 +182,6 @@ class RouteTableViewCell: UITableViewCell {
         
         //Set up distance label
         distanceLabel.frame = CGRect(x: stopLabels[0].frame.maxX + space, y: stopLabels[0].frame.minY, width: 90, height: 20)
-        distanceLabel.font = UIFont(name: "SFUIText-Regular", size: 12.0)
-        distanceLabel.textColor = .distanceLabelColor
-        distanceLabel.text = "\(distance) mi away"
-        distanceLabel.sizeToFit()
-        
-        self.addSubview(distanceLabel)
-        
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        
     }
 
 }
