aae640de7c217f42aee58e1ceba74f4d73b0ecb7
Monica Ong <myo3@cornell.edu>
2017-03-05 15:18:20 -0500

    Fix merge conflicts in pbxproj

---
+++ TCAT/TCAT/Model/Direction.swift
@@ -0,0 +1,79 @@
+//
+//  Direction.swift
+//  TCAT
+//
+//  Created by Monica Ong on 2/12/17.
+//  Copyright © 2017 cuappdev. All rights reserved.
+//
+
+import UIKit
+
+protocol Direction {
+    var departureTime: Date {get set}
+    var departurePlace: String {get set}
+    var departureDescription: String {get} //e.g. "Walk to Statler", "Board at Statler", "Debark at Ithaca Commons"
+}
+
+/* To get string version of Bound
+  * let inbound: String = Bound.inbound.rawValue  //"inbound"
+  * let outbound: String = Bound.inbound.rawValue //"outbound"
+*/
+enum Bound: String{
+    case inbound, outbound
+}
+
+class BoardDirection: Direction {
+    
+    var routeNumber: Int
+    var bound: Bound
+    var stops: [String]
+    
+    var departureTime: Date
+    var departurePlace: String
+    var departureDescription: String {
+        return "Board at \(departurePlace)"
+    }
+    
+    var arrivalTime: Date
+    var arrivalPlace: String
+    var arrivalDescription: String {
+        return "Debark at \(arrivalPlace)"
+    }
+    
+    /*To extract travelTime's times in day, hour, and minute units:
+     * let days: Int = travelTime.day
+     * let hours: Int = travelTime.hour
+     * let minutes: Int = travelTime.minute
+     */
+    var travelTime: DateComponents {
+        return Calendar.current.dateComponents([.hour, .minute, .day], from: departureTime , to: arrivalTime)
+    }
+    
+    init(routeNumber: Int, bound: Bound, stops: [String] = [], departureTime: Date, departurePlace: String, arrivalTime: Date, arrivalPlace: String) {
+        self.routeNumber = routeNumber
+        self.bound = bound
+        self.stops = stops
+        self.departureTime = departureTime
+        self.departurePlace = departurePlace
+        self.arrivalTime = arrivalTime
+        self.arrivalPlace = arrivalPlace
+    }
+
+}
+
+class WalkDirection: Direction {
+    
+    var departureTime: Date
+    var departurePlace: String
+    var departureDescription: String {
+        return "Walk to \(departurePlace)"
+    }
+    var travelDistance: Double
+    
+    
+    init(departureTime: Date, departurePlace: String, travelDistance: Double) {
+        self.departureTime = departureTime
+        self.departurePlace = departurePlace
+        self.travelDistance = travelDistance
+    }
+}
---
+++ TCAT/TCAT/Model/Route.swift
@@ -0,0 +1,73 @@
+//
+//  Route.swift
+//  TCAT
+//
+//  Description: 
+//      Data model to represent both route options screen (Monica) and route detail screen (Matt)
+//
+//  Note:
+//      - mainStops is for route options screen (Monica) and directions is for route detail screen (Matt)
+//      - departureTime and arrivalTime are for the entire route, while each direction has their own departure (and possibly arrival time)
+//
+//          Example: (One Route Stop Details screen)
+//Route:
+//    departureTime = 7:21 PM
+//    arrivalTime = 7:39 PM
+//    timeUntilDeparture = 5 min
+//    directions = [WalkDirection1, BoardDirection, WalkDirection2]
+//    mainStops = ["BakerFlagpole", "Angry Mom Records"]
+//
+//WalkDirection1:
+//    departureTime = 7:21 PM
+//    departurePlace = "Statler Hall"
+//    departureDescription = "Walk to Statler Hall"
+//    travelDistance = 0.2
+//
+//BoardDirection:
+//    routeNumber = 32
+//    bound = inbound
+//    stops = ["Bus Stop Name", "Bus Stop Name", "Bus Stop Name"]
+//    departureTime = 7:24 PM
+//    departurePlace = "Statler Hall"
+//    arrivalTime = 7:36 PM
+//    arrivalPlace = "Ithaca Commons"
+//    arrivalDescription = "Debark at Ithaca Commons"
+//    travelTime = 12 min
+//
+//WalkDirection2:
+//    departureTime = 7:39 PM
+//    departurePlace = "Angry Mom Records"
+//    departureDescription = "Walk to Angry Mom Records"
+//    travelDistance = 0.2
+//
+//  Created by Monica Ong on 2/12/17.
+//  Copyright © 2017 cuappdev. All rights reserved.
+//
+
+import UIKit
+
+class Route: NSObject {
+    
+    var departureTime: Date
+    var arrivalTime: Date
+    
+    /*To extract timeUntilDeparture's times in day, hour, and minute units:
+     * let days: Int = timeUntilDeparture.day
+     * let hours: Int = timeUntilDeparture.hour
+     * let minutes: Int = timeUntilDeparture.minute
+     */
+    var timeUntilDeparture: DateComponents {
+        let now = Date() //curent date
+        return Calendar.current.dateComponents([.hour, .minute, .day], from: now , to: departureTime)
+    }
+    
+    var directions: [Direction]
+    var mainStops: [String]
+    
+    init(departureTime: Date, arrivalTime: Date, directions: [Direction], mainStops: [String]) {
+        self.departureTime = departureTime
+        self.arrivalTime = arrivalTime
+        self.directions = directions
+        self.mainStops = mainStops
+    }
+}
