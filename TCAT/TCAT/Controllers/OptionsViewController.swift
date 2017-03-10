5e08f7f6851907184801a6756f3b49c079809f84
Monica Ong <myo3@cornell.edu>
2017-03-10 14:36:30 -0500

    Set up route cells

--- TCAT/TCAT/Controllers/OptionsViewController.swift
+++ TCAT/TCAT/Controllers/OptionsViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 /* N2SELF:
-  * fix spacing of cells
   * stop tableview from scrolling beyond top & bottom
-  * add custom bus button and dots & lines
+  * need to test departure time
+  * Route model > change mainStopNums to mainBusNums
+  * make font of busIcon = SFU
  */
 
 class OptionsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -29,7 +30,7 @@ class OptionsViewController: UIViewController, UITableViewDelegate, UITableViewD
         //Set up navigation bar
         title = "Route Options"
         //Set up route selection view
-        routeSelection = routeSelectionView(frame: CGRect(x: 0, y: (navigationController?.navigationBar.frame.height)! + UIApplication.shared.statusBarFrame.size.height, width: view.frame.width, height: 150))
+        routeSelection = routeSelectionView(frame: CGRect(x: 0, y: (navigationController?.navigationBar.frame.height ?? 0) + UIApplication.shared.statusBarFrame.size.height, width: view.frame.width, height: 150))
         routeSelection.backgroundColor = .lineColor
         routeSelection.positionViews()
         var newRSFrame = routeSelection.frame
@@ -38,11 +39,12 @@ class OptionsViewController: UIViewController, UITableViewDelegate, UITableViewD
         view.addSubview(routeSelection)
         
         //Set up table view
-        routeResults = UITableView(frame: CGRect(x: 0, y: routeSelection.frame.maxY, width: view.frame.width, height: view.frame.height - routeSelection.frame.height))
+        routeResults = UITableView(frame: CGRect(x: 0, y: routeSelection.frame.maxY, width: view.frame.width, height: view.frame.height - routeSelection.frame.height - (navigationController?.navigationBar.frame.height ?? 0) - UIApplication.shared.statusBarFrame.size.height))
         routeResults.delegate = self
         routeResults.dataSource = self
         routeResults.separatorStyle = .none
         routeResults.allowsSelection = false
+        routeResults.backgroundColor = .routeResultsBackColor
 
         view.addSubview(routeResults)
         
@@ -84,10 +86,11 @@ class OptionsViewController: UIViewController, UITableViewDelegate, UITableViewD
             cell = RouteTableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: identifier)
         }
         
+        print("Cell \(indexPath.row)")
         cell?.departureTime = routes[indexPath.row].departureTime
         cell?.arrivalTime = routes[indexPath.row].arrivalTime
         cell?.stops = routes[indexPath.row].mainStops
-        cell?.stopNums = routes[indexPath.row].mainStopsNums
+        cell?.busNums = routes[indexPath.row].mainStopsNums
         cell?.distance = routes[indexPath.row].travelDistance
         cell?.setData()
         
@@ -105,24 +108,31 @@ class OptionsViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         let header = view as! UITableViewHeaderFooterView
+        header.contentView.backgroundColor = .routeResultsBackColor
         header.textLabel?.font = UIFont(name: "SFUIText-Regular", size: 14.0)
         header.textLabel?.textColor = UIColor.headerTitleColor
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let travelTimeHeight: CGFloat = 20.33
-        let pinHeight: CGFloat = 33.0
-        let arrowHeight: CGFloat = 6.0
-        let space: CGFloat = 18.0
+        let spaceYTimeLabelFromSuperviewTop: CGFloat = 18.0
+        let travelTimeHeight: CGFloat = 17.0
+        let spaceYTimeLabelAndDot: CGFloat = 26.0
+        let heightDot: CGFloat = 8.0
+        let lineLengthYBtDots: CGFloat = 21.0
         
-        let numOfArrows = CGFloat(routes[indexPath.row].mainStopsNums.count-1)
-        let numOfPins = CGFloat(routes[indexPath.row].mainStopsNums.count)
+        let spaceBtDotAndLineDot: CGFloat = 17.0
+        let heightLineDot: CGFloat = 16.0
+        let spaceYToCellBorder: CGFloat = 18.0
+        let cellBorderWidthY: CGFloat = 0.75
+        let cellSpaceWidthY: CGFloat = 4.0
         
-        let totalPinHeight = numOfPins*pinHeight
-        let totalPinSpacingHeight = numOfArrows*space*2
-        let totalArrowHeight = numOfArrows*arrowHeight
+        let numOfDots = routes[indexPath.row].mainStops.count - 1 //1 less b/c last dot is line dot
+        let numOfLinesBtDots = numOfDots - 1
         
-        return space + travelTimeHeight + space + totalPinHeight + totalPinSpacingHeight + totalArrowHeight  + space
+        let  headerHeight = spaceYTimeLabelFromSuperviewTop + travelTimeHeight + spaceYTimeLabelAndDot
+        let dotsHeight = CGFloat(numOfDots)*heightDot + CGFloat(numOfLinesBtDots)*lineLengthYBtDots + spaceBtDotAndLineDot + heightLineDot
+        let footerHeight = spaceYToCellBorder + cellBorderWidthY + cellSpaceWidthY
+        return (headerHeight + dotsHeight + footerHeight)
     }
     
 
