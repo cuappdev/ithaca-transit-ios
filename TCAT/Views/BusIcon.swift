353ee4af24e0007b090c808d00a814e7a79526fd
Monica Ong <myo3@cornell.edu>
2017-03-19 14:51:11 -0400

    Changed file directory to match master

---
+++ TCAT/Views/BusIcon.swift
@@ -0,0 +1,56 @@
+//
+//  BusIcon.swift
+//  TCAT
+//
+//  Created by Matthew Barker on 2/26/17.
+//  Copyright © 2017 cuappdev. All rights reserved.
+//
+
+import UIKit
+
+enum BusIconSize: String {
+    case small, large
+}
+
+class BusIcon: UIView {
+    
+    var color: UIColor = UIColor(red: 7 / 255, green: 157 / 255, blue: 220 / 255, alpha: 1)
+    var number: Int = 99
+    
+    var label: UILabel!
+    var image: UIImageView!
+    
+    init(size: BusIconSize, number: Int) {
+        switch size {
+        case .small : super.init(frame: CGRect(x: 0, y: 0, width: 48, height: 24))
+        case .large : super.init(frame: CGRect(x: 0, y: 0, width: 72, height: 36))
+        }
+        
+        self.number = number
+        self.backgroundColor = color
+        self.layer.cornerRadius = size == .large ? 8 : 4
+        
+        image = UIImageView(image: UIImage(named: "bus"))
+        let constant: CGFloat = size == .large ? 0.9 : 0.6
+        image.frame.size = CGSize(width: image.frame.width * constant, height: image.frame.height * constant)
+        image.tintColor = .white
+        image.center = center
+        image.center.x -= size == .large ? image.frame.width / 2 - 2 : image.frame.width / 2
+        addSubview(image)
+        
+        label = UILabel()
+        label.text = "\(number)"
+        label.font = UIFont.systemFont(ofSize: size == .large ? 21 : 15, weight: UIFontWeightSemibold)
+        label.textColor = .white
+        label.sizeToFit()
+        label.center = center
+        label.center.x += size == .large ? label.frame.width / 2 + 4 : label.frame.width / 2
+        addSubview(label)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
---
+++ TCAT/Views/DirectionCircle.swift
@@ -0,0 +1,53 @@
+//
+//  File.swift
+//  TCAT
+//
+//  Created by Matthew Barker on 3/1/17.
+//  Copyright © 2017 cuappdev. All rights reserved.
+//
+
+import UIKit
+
+enum CircleType: String {
+    case standardOn, standardOff, finishOn, finishOff, busStop
+}
+
+class DirectionCircle: UIView {
+    
+    var type: CircleType!
+    let blue = UIColor(red: 7 / 255, green: 157 / 255, blue: 220 / 255, alpha: 1)
+
+    
+    init(_ type: CircleType) {
+        
+        let isStandard = type != .finishOn && type != .finishOff
+        
+        let radius = !isStandard ? 16 : 8
+        super.init(frame: CGRect(x: 0, y: 0, width: radius, height: radius))
+        
+        layer.cornerRadius = frame.size.width / 2
+        clipsToBounds = true
+        
+        if !isStandard {
+            backgroundColor = .clear
+            layer.borderColor = (type == .finishOn) ? UIColor.blue.cgColor : UIColor.gray.cgColor
+            layer.borderWidth = 1.0
+            let innerCircle = DirectionCircle(type == .finishOn ? .standardOn : .standardOff)
+            innerCircle.center = center
+            addSubview(innerCircle)
+        } else {
+            backgroundColor = type == .standardOff ? .gray : blue
+            if type == .busStop {
+                backgroundColor = .white
+                layer.borderColor = blue.cgColor
+                layer.borderWidth = 1.0
+            }
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
