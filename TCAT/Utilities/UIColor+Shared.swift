28f8aa7435945b3bb9bad1dd03e6c28fd23d1ead
Annie Cheng <ac962@cornell.edu>
2017-03-22 18:07:34 -0400

    Fix folder and file hierarchy after merge

--- TCAT/Utilities/UIColor+Shared.swift
+++ TCAT/Utilities/UIColor+Shared.swift
@@ -17,5 +17,14 @@ extension UIColor {
     @nonobjc static let stopNumColor2 = UIColor(red: 255/255, green: 97/255, blue: 116/255, alpha: 1.0)
     @nonobjc static let distanceLabelColor = UIColor(red: 187/255, green: 187/255, blue: 187/255, alpha: 1.0)
     @nonobjc static let pinColor = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
+    @nonobjc static let tcatBlue = UIColor.colorFromCode(0x3A96FF, alpha: 1)
     
+    // Get color from hex code
+    public static func colorFromCode(_ code: Int, alpha: CGFloat) -> UIColor {
+        let red = CGFloat(((code & 0xFF0000) >> 16)) / 255
+        let green = CGFloat(((code & 0xFF00) >> 8)) / 255
+        let blue = CGFloat((code & 0xFF)) / 255
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
 }
