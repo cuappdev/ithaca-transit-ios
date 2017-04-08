83026f9d2d315f22e5befe8b4e17863269086ce7
Monica Ong <myo3@cornell.edu>
2017-04-08 00:40:48 -0400

    Fixed xcodeproj. Oragnized files to reflect folder organization in xcodeproj

--- Pods/Target Support Files/Pods-TCAT/Pods-TCAT-frameworks.sh
+++ Pods/Target Support Files/Pods-TCAT/Pods-TCAT-frameworks.sh
@@ -89,10 +89,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/TRON/TRON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/TRON/TRON.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
