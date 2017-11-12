4f07e8cabc6f1b1c7da8465120db72d2fc6517dc
mattbarker016 <mjb485@cornell.edu>
2017-11-12 15:58:48 -0500

    fix

--- TCAT/AppDelegate.swift
+++ TCAT/AppDelegate.swift
@@ -76,7 +76,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the inactive state; here y/Users/mattbarker016ou can undo many of the changes made on entering the background.
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
