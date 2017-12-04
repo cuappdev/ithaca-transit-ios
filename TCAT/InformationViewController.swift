9e07970f8fb94c770ac5992892a2f34957ca5a4f
Ji Hwan Seung <jiseung95@gmail.com>
2017-12-04 16:47:57 -0500

    page compatible with multiple screens

--- TCAT/InformationViewController.swift
+++ TCAT/InformationViewController.swift
@@ -19,7 +19,7 @@ class InformationViewController: UIViewController {
     var tcatImage = UIImageView()
     var sendFeedbackButton = UIButton()
     var visitWebsiteButton = UIButton()
-    var cancelButton = UIButton(type: .system)
+    var backButton = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,29 +27,30 @@ class InformationViewController: UIViewController {
         title = "About Us"
         
         view.backgroundColor = UIColor.tableBackgroundColor
+        navigationController?.navigationBar.tintColor = .primaryTextColor
         
         view.addSubview(titleLabel)
-        view.addSubview(appDevImage)
-        view.addSubview(appDevTitle)
         view.addSubview(descriptionLabel)
         view.addSubview(sendFeedbackButton)
+        view.addSubview(visitWebsiteButton)
         view.addSubview(tcatImage)
-        view.addSubview(tcatQuoteText)
         
-        cancelButton.addTarget(self, action: #selector(cancelButtonClicked), for: .touchUpInside)
-        let attributedString = NSMutableAttributedString(string: "Cancel")
+        backButton.addTarget(self, action: #selector(backButtonClicked), for: .touchUpInside)
+        backButton.setImage(UIImage(named: "back"), for: .normal)
+        let attributedString = NSMutableAttributedString(string: "  Book")
         attributedString.addAttribute(NSAttributedStringKey.baselineOffset, value: 0.3, range: NSMakeRange(0, attributedString.length))
-        cancelButton.setAttributedTitle(attributedString, for: .normal)
-        cancelButton.sizeToFit()
-        let cancelButtonItem = UIBarButtonItem(customView: cancelButton)
-        navigationItem.setLeftBarButton(cancelButtonItem, animated: false)
+        backButton.setAttributedTitle(attributedString, for: .normal)
+        backButton.sizeToFit()
+        let backButtonItem = UIBarButtonItem(customView: backButton)
+        navigationItem.setLeftBarButton(backButtonItem, animated: false)
         
         tcatImage.image = UIImage(named: "tcatbus")
         tcatImage.snp.makeConstraints { (make) in
             make.centerX.equalToSuperview()
-            make.top.equalTo(topLayoutGuide.snp.bottom).offset(32)
-            make.bottom.equalTo(titleLabel.snp.top).offset(-28)
-            make.width.equalTo(tcatImage.snp.height).multipliedBy(2)
+            make.top.equalTo(topLayoutGuide.snp.bottom).offset(44)
+            make.width.equalTo(tcatImage.snp.height).multipliedBy(2.5)
+            make.leading.equalToSuperview().offset(40)
+            make.trailing.equalToSuperview().offset(-40)
         }
         
         titleLabel.font = UIFont(name: FontNames.SanFrancisco.Medium, size: 16)
@@ -57,8 +58,10 @@ class InformationViewController: UIViewController {
         titleLabel.text = "Made by AppDev"
         titleLabel.backgroundColor = .clear
         titleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(tcatImage.snp.bottom).offset(44)
             make.centerX.equalToSuperview()
             make.bottom.equalTo(descriptionLabel.snp.top).offset(-12)
+            make.height.equalTo(19)
         }
         
         descriptionLabel.font = UIFont(name: FontNames.SanFrancisco.Regular, size: 14)
@@ -68,53 +71,44 @@ class InformationViewController: UIViewController {
         descriptionLabel.backgroundColor = .clear
         descriptionLabel.textAlignment = .center
         descriptionLabel.snp.makeConstraints { (make) in
-            make.bottom.equalTo(view.snp.centerY).offset(-10)
+            make.height.equalTo(34)
+//            make.bottom.equalTo(view.snp.centerY).offset(-10)
             make.centerX.equalToSuperview()
         }
         
         sendFeedbackButton.addTarget(self, action: #selector(openBugReportForm), for: .touchUpInside)
-        sendFeedbackButton.setTitle("Send feedback", for: .normal)
-        sendFeedbackButton.setTitleColor(.white, for: .normal)
-        sendFeedbackButton.titleLabel?.font = UIFont(name: FontNames.SanFrancisco.Medium, size: 16)
-        sendFeedbackButton.backgroundColor = UIColor.tcatBlueColor
-        sendFeedbackButton.layer.cornerRadius = 5.0
+        sendFeedbackButton.setTitle("Send Feedback", for: .normal)
+        sendFeedbackButton.setTitleColor(UIColor.tcatBlueColor, for: .normal)
+        sendFeedbackButton.titleLabel?.font = UIFont(name: FontNames.SanFrancisco.Regular, size: 14)
+        sendFeedbackButton.backgroundColor = UIColor.white
+        sendFeedbackButton.layer.borderColor = UIColor.lineDarkColor.cgColor
+        sendFeedbackButton.layer.borderWidth = 0.5
         sendFeedbackButton.snp.makeConstraints { (make) in
             make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().offset(-85)
-            make.width.equalTo(135)
+            make.leading.equalToSuperview().offset(-0.5)
+            make.trailing.equalToSuperview().offset(0.5)
             make.height.equalTo(44)
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(37)
         }
         
-        appDevTitle.font = UIFont(name: FontNames.SanFrancisco.Medium, size: 10)
-        appDevTitle.textColor = UIColor.secondaryTextColor
-        appDevTitle.text = "Created by Cornell App Development"
-        appDevTitle.snp.makeConstraints { (make) in
-            make.bottom.equalToSuperview().offset(-10)
-            make.centerX.equalToSuperview().offset(15)
-            make.width.equalTo(180)
-            make.height.equalTo(appDevImage.snp.height)
+        visitWebsiteButton.addTarget(self, action: #selector(openTeamWebsite), for: .touchUpInside)
+        visitWebsiteButton.setTitle("Visit Our Website", for: .normal)
+        visitWebsiteButton.setTitleColor(UIColor.primaryTextColor, for: .normal)
+        visitWebsiteButton.titleLabel?.font = UIFont(name: FontNames.SanFrancisco.Regular, size: 14)
+        visitWebsiteButton.backgroundColor = .white
+        visitWebsiteButton.layer.borderColor = UIColor.lineDarkColor.cgColor
+        visitWebsiteButton.layer.borderWidth = 0.5
+        visitWebsiteButton.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(sendFeedbackButton)
+            make.trailing.equalTo(sendFeedbackButton)
+            make.height.equalTo(44)
+            make.top.equalTo(sendFeedbackButton.snp.bottom).offset(-0.5)
         }
-        
-        appDevImage.image = UIImage(named: "appdev_logo")
-        appDevImage.snp.makeConstraints { (make) in
-            make.bottom.equalTo(appDevTitle.snp.bottom)
-            make.trailing.equalTo(appDevTitle.snp.leading).offset(-10)
-            make.width.equalTo(20)
-            make.height.equalTo(20)
-        }
-        
     }
     
-    @objc func cancelButtonClicked() {
+    @objc func backButtonClicked() {
         self.dismiss(animated: true, completion: nil)
-        
-//        let transition: CATransition = CATransition()
-//        transition.duration = 0.5
-//        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-//        transition.type = kCATransitionPush
-//        transition.subtype = kCATransitionFromBottom
-//        self.navigationController!.view.layer.add(transition, forKey: kCATransition)
-//        self.navigationController?.popViewController(animated: false)
     }
     
     @objc func openBugReportForm() {
@@ -123,4 +117,9 @@ class InformationViewController: UIViewController {
         UIApplication.shared.keyWindow?.presentInApp(safariViewController)
     }
     
+    @objc func openTeamWebsite() {
+        let siteURL = "http://www.cornellappdev.com"
+        let safariViewController = SFSafariViewController(url: URL(string: siteURL)!)
+        UIApplication.shared.keyWindow?.presentInApp(safariViewController)
+    }
 }
