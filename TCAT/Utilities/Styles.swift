660e5320ebeaa133215c321928465c2748742266
mattbarker016 <mjb485@cornell.edu>
2018-10-13 19:01:37 -0400

    Fixed bugs with SF font

--- TCAT/Utilities/Styles.swift
+++ TCAT/Utilities/Styles.swift
@@ -16,18 +16,19 @@ import UIKit
 struct Fonts {
     
     struct SanFrancisco {
-        static let regular = "SFUIText-Regular"
-        static let medium = "SFUIText-Medium"
-        static let semibold = "SFUIText-Semibold"
-        static let bold = "SFUIText-Bold"
+        static let regular = "SFProDisplay-Regular"
+        static let medium = "SFProDisplay-Medium"
+        static let semibold = "SFProDisplay-Semibold"
+        static let bold = "SFProDisplay-Bold"
     }
     
     struct System {
+        // Placeholder for init function below, not actually used.
         static let systemFontSize: CGFloat = 14 // Same as UIFont.systemFontSize
+        
         static let regular = UIFont.systemFont(ofSize: systemFontSize, weight: .regular).fontName
         static let semibold = UIFont.systemFont(ofSize: systemFontSize, weight: .semibold).fontName
         static let bold = UIFont.systemFont(ofSize: systemFontSize, weight: .bold).fontName
-        // Was previously UIFont.boldSystemFont(ofSize: UIFont.systemFontSize)
     }
     
 }
@@ -36,7 +37,34 @@ extension UIFont {
     
     /// Generate fonts for app usage
     static func style(_ name: String, size: CGFloat) -> UIFont {
+        
         return UIFont(name: name, size: size)!
+        
+        let sanFrancisco: [String] = [
+            Fonts.SanFrancisco.regular,
+            Fonts.SanFrancisco.medium,
+            Fonts.SanFrancisco.semibold,
+            Fonts.SanFrancisco.bold
+        ]
+        
+        // Installed San Francisco Font
+        if sanFrancisco.contains(name) {
+            print(UIFont.fontNames(forFamilyName: "SF Pro Display"))
+            let fontStringArray = name.split(separator: "-")
+            let textStyle = fontStringArray.last ?? ""
+            let descriptor = UIFontDescriptor(fontAttributes: [
+                UIFontDescriptor.AttributeName.name : name,
+                /*UIFontDescriptor.AttributeName.family : "SF Pro Display",*/
+                /*UIFontDescriptor.AttributeName.textStyle : textStyle*/
+            ])
+            return UIFont(descriptor: descriptor, size: size)
+        }
+        
+        // System Font
+        else {
+            
+        }
+        
     }
     
 }
