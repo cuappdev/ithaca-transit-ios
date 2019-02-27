d6ec3f0d4f6620fb06732af5039efa9b1f108726
mattbarker016 <mjb485@cornell.edu>
2019-02-27 17:13:28 -0500

    New Place model created, older models refactored for migration use

--- TCAT/Model/Place.swift
+++ TCAT/Model/Place.swift
@@ -8,32 +8,79 @@
 
 import UIKit
 
-class Place: NSObject, NSCoding, Codable {
+enum PlaceType: String, Codable {
+    case busStop, googlePlace, unknown
+}
+
+class Place: NSObject, Codable {
 
     var name: String
+    var type: PlaceType
     
-    private let nameKey = "name"
+    // Additional description of the place (e.g. address, "Bus Stop")
+    private var placeDescription: String?
+    
+    // Metadata related to the place (e.g. Google Place ID)
+    var placeIdentifier: String?
+    
+    var latitude: Double?
+    var longitude: Double?
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case type
+        case placeDescription = "detail"
+        case placeIdentifier = "placeID"
+        case latitude = "lat"
+        case longitude = "long"
+    }
     
     init(name: String) {
         self.name = name
+        self.type = .unknown
+    }
+    
+    /// Initializer for Google Places
+    convenience init(name: String, placeDescription: String, placeIdentifier: String) {
+        self.init(name: name)
+        self.type = .googlePlace
+        self.placeDescription = placeDescription
+        self.placeIdentifier = placeIdentifier
+    }
+    
+    /// Initializer for Bus Stops
+    convenience init(name: String, latitude: Double, longitude: Double) {
+        self.init(name: name)
+        self.type = .busStop
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    // MARK: Functions
+    
+    override var description: String {
+        return """
+        name: \(name),
+        type: \(type),
+        description: \(String(describing: placeDescription)),
+        identifier: \(String(describing: placeIdentifier))
+        latitude: \(String(describing: latitude)),
+        longitiude: \(String(describing: longitude))
+        """
     }
     
     override func isEqual(_ object: Any?) -> Bool {
         guard let object = object as? Place else {
             return false
         }
-        
+        if let identifier = object.placeIdentifier {
+            return identifier == placeIdentifier
+        }
         return object.name == name
     }
     
-    // MARK: NSCoding
-    
-    required init(coder aDecoder: NSCoder) {
-        name = aDecoder.decodeObject(forKey: nameKey) as! String
-    }
-    
-    public func encode(with aCoder: NSCoder) {
-        aCoder.encode(self.name, forKey: nameKey)
+    func getDescription() -> String {
+        return (type == .googlePlace) ? (placeDescription ?? "") : ("Bus Stop")
     }
     
 }
