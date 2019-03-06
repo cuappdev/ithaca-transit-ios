ac68149bc0c1e1dadfcecf6d2f73ad1b21fb8fe7
mattbarker016 <mjb485@cornell.edu>
2019-03-06 02:36:38 -0500

    Removed dummy data func call

--- TCAT/Controllers/ServiceAlertsViewController.swift
+++ TCAT/Controllers/ServiceAlertsViewController.swift
@@ -52,8 +52,7 @@ class ServiceAlertsViewController: UIViewController {
         
         updateConstraints()
         
-        // getServiceAlerts()
-        createDummyData()
+        getServiceAlerts()
     }
     
     func updateConstraints() {
