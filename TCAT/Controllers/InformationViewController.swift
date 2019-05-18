a7fda3245660d608309b7d703ceccd46bcb8bece
Omar Rasheed <oyr2@cornell.edu>
2019-05-18 04:35:37 -0400

    Add static views as headerView of tableView

--- TCAT/Controllers/InformationViewController.swift
+++ TCAT/Controllers/InformationViewController.swift
@@ -10,19 +10,20 @@ import MessageUI
 import SafariServices
 import UIKit
 
-class InformationViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, MFMailComposeViewControllerDelegate {
+class InformationViewController: UIViewController {
 
-    private var appDevImage = UIImageView()
-    private var appDevTitle = UILabel()
-    private var descriptionLabel = UILabel()
-    private var dismissButton = UIButton(type: .system)
-    private var hiddenLabel = UILabel()
-    private var sendFeedbackButton = UIButton()
-    private var tableView = UITableView(frame: .zero, style: .grouped)
-    private var tcatImage = UIImageView()
-    private var tcatQuoteText = UILabel()
-    private var titleLabel = UILabel()
-    private var visitWebsiteButton = UIButton()
+    private let appDevImage = UIImageView()
+    private let appDevTitle = UILabel()
+    private let descriptionLabel = UILabel()
+    private let dismissButton = UIButton(type: .system)
+    private var headerView: UIView!
+    private let hiddenLabel = UILabel()
+    private let sendFeedbackButton = UIButton()
+    private let tableView = UITableView(frame: .zero, style: .grouped)
+    private let tcatImage = UIImageView()
+    private let tcatQuoteText = UILabel()
+    private let titleLabel = UILabel()
+    private let visitWebsiteButton = UIButton()
 
     private var content: [[(name: String, action: Selector)]] = [
 
@@ -42,20 +43,64 @@ class InformationViewController: UIViewController, UITableViewDataSource, UITabl
 
     ]
 
+    private let descriptionLabelSize = CGSize(width: 167, height: 34)
+    private let descriptionLabelTopOffset: CGFloat = 12
+    private let headerWidth = UIScreen.main.bounds.width
+    /** Represents the total height of the headerView given the width of the phone screen. Most of the variables
+     used are static except for the tcat image height, which is dependent on the width of the user's screen.
+     The extra 37 is used for bottom inset padding for the headerView, but I felt it would be unnecessary to
+     create a variable for it */
+    private var headerHeight: CGFloat {
+        return tcatImageTopOffset + tcatImageSize.height + titleLabelTopOffset + titleLabelSize.height + descriptionLabelTopOffset + descriptionLabelSize.height + 37
+    }
+    private var tcatImageSize: CGSize { return CGSize(width: headerWidth-80, height: (headerWidth - 80) * 0.4) }
+    private let tcatImageTopOffset: CGFloat = 44
+    private let titleLabelSize = CGSize(width: 240, height: 20)
+    private let titleLabelTopOffset: CGFloat = 44
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let payload = AboutPageOpenedPayload()
+        Analytics.shared.log(payload)
 
         title = Constants.Titles.aboutUs
 
         view.backgroundColor = Colors.backgroundWash
         navigationController?.navigationBar.tintColor = Colors.primaryText
 
-        view.addSubview(hiddenLabel)
-        view.addSubview(tcatImage)
-        view.addSubview(titleLabel)
-        view.addSubview(descriptionLabel)
-        view.addSubview(tableView)
+        setupTableView()
 
+        setupConstraints()
+    }
+
+    func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.Cells.informationCellIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = Colors.backgroundWash
+        tableView.separatorColor = Colors.dividerTextField
+        tableView.showsVerticalScrollIndicator = false
+
+        setupHeaderView()
+        tableView.tableHeaderView = headerView
+
+        view.addSubview(tableView)
+    }
+
+    func setupHeaderView() {
+        headerView = UIView(frame: CGRect(x: 0, y: 0, width: headerWidth, height: headerHeight))
+
+        setupDismissButton()
+        setupHiddenLabel()
+        setupTcatImage()
+        setupTitleLabel()
+        setupDescriptionLabel()
+
+        setupHeaderViewConstraints()
+    }
+
+    func setupDismissButton() {
         dismissButton.addTarget(self, action: #selector(dismissTapped), for: .touchUpInside)
         dismissButton.setTitle(Constants.Buttons.done, for: .normal)
         navigationItem.rightBarButtonItem?.setTitleTextAttributes(
@@ -63,117 +108,72 @@ class InformationViewController: UIViewController, UITableViewDataSource, UITabl
         )
         let backButtonItem = UIBarButtonItem(customView: dismissButton)
         navigationItem.setRightBarButton(backButtonItem, animated: false)
+    }
 
+    func setupHiddenLabel() {
         hiddenLabel.font = .getFont(.regular, size: 16)
         hiddenLabel.textColor = Colors.primaryText
         hiddenLabel.text = Constants.InformationView.magicSchoolBus
         hiddenLabel.textAlignment = .center
         hiddenLabel.backgroundColor = .clear
-        hiddenLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(86)
-            make.centerX.equalToSuperview()
-            make.height.equalTo(20)
-        }
+        headerView.addSubview(hiddenLabel)
+    }
 
+    func setupTcatImage() {
         tcatImage.image = UIImage(named: "tcat")
         tcatImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(busTapped)))
         tcatImage.isUserInteractionEnabled = true
-        tcatImage.snp.makeConstraints { (make) in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(44)
-            make.width.equalTo(tcatImage.snp.height).multipliedBy(2.5)
-            make.leading.equalToSuperview().offset(40)
-            make.trailing.equalToSuperview().offset(-40)
-        }
+        headerView.addSubview(tcatImage)
+    }
 
+    func setupTitleLabel() {
         titleLabel.font = .getFont(.medium, size: 16)
         titleLabel.textColor = Colors.primaryText
         titleLabel.text = Constants.InformationView.madeBy
         titleLabel.backgroundColor = .clear
-        titleLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(tcatImage.snp.bottom).offset(44)
-            make.centerX.equalToSuperview()
-            make.bottom.equalTo(descriptionLabel.snp.top).offset(-12)
-            make.height.equalTo(20)
-        }
+        headerView.addSubview(titleLabel)
+    }
 
+    func setupDescriptionLabel() {
         descriptionLabel.font = .getFont(.regular, size: 14)
         descriptionLabel.textColor = Colors.primaryText
         descriptionLabel.text = Constants.InformationView.appDevDescription
         descriptionLabel.numberOfLines = 0
         descriptionLabel.backgroundColor = .clear
         descriptionLabel.textAlignment = .center
-        descriptionLabel.snp.makeConstraints { (make) in
-            make.height.equalTo(34)
+        headerView.addSubview(descriptionLabel)
+    }
+
+    func setupHeaderViewConstraints() {
+        hiddenLabel.snp.makeConstraints { make in
+            make.center.equalTo(tcatImage)
+            make.size.equalTo(hiddenLabel.intrinsicContentSize)
+        }
+
+        tcatImage.snp.makeConstraints { make in
+            make.top.equalTo(headerView).offset(tcatImageTopOffset)
+            make.size.equalTo(tcatImageSize)
             make.centerX.equalToSuperview()
         }
 
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.Cells.informationCellIdentifier)
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.backgroundColor = Colors.backgroundWash
-        tableView.separatorColor = Colors.dividerTextField
-        tableView.isScrollEnabled = false
-        tableView.showsVerticalScrollIndicator = false
-
-        tableView.snp.makeConstraints { (make) in
-            make.top.equalTo(descriptionLabel.snp.bottom)
-            make.left.equalTo(view)
-            make.right.equalTo(view)
-            make.bottom.equalTo(view)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(tcatImage.snp.bottom).offset(titleLabelTopOffset)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(titleLabelSize)
         }
 
-        let payload = AboutPageOpenedPayload()
-        Analytics.shared.log(payload)
-
-    }
-
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .default
-    }
-
-    // MARK: Table View Data Source
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return content.count
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return content[section].count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        // Initalize and format cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.Cells.informationCellIdentifier, for: indexPath)
-        cell.selectionStyle = .none
-        cell.backgroundColor = Colors.white
-        cell.textLabel?.textColor = Colors.primaryText
-        cell.textLabel?.font = .getFont(.regular, size: 14)
-        cell.textLabel?.textAlignment = .center
-
-        // Set cell content
-        cell.textLabel?.text = content[indexPath.section][indexPath.row].name
-
-        // Set custom formatting based on cell
-        switch (indexPath.section, indexPath.row) {
-
-        case (1, 1): // Send Feedback
-            cell.textLabel?.textColor = Colors.tcatBlue
-        default:
-            break
-
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(descriptionLabelTopOffset)
+            make.size.equalTo(descriptionLabelSize)
+            make.centerX.equalToSuperview()
         }
-
-        return cell
-
     }
 
-    // MARK: Table View Delegate
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selector = content[indexPath.section][indexPath.row].action
-        performSelector(onMainThread: selector, with: nil, waitUntilDone: false)
+    func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
     }
 
     // MARK: Functions
@@ -188,61 +188,6 @@ class InformationViewController: UIViewController, UITableViewDataSource, UITabl
         let onboardingViewController = OnboardingViewController(initialViewing: false)
         let navigationController = OnboardingNavigationController(rootViewController: onboardingViewController)
         present(navigationController, animated: true)
-    }
-
-    @objc func sendFeedback() {
-
-        let emailAddress = Constants.App.contactEmailAddress
-
-        if MFMailComposeViewController.canSendMail() {
-
-            let mailComposerVC = MFMailComposeViewController()
-            mailComposerVC.mailComposeDelegate = self
-            var didRetrieveLog = false
-
-            if let logs = retrieveLogs() {
-                mailComposerVC.addAttachmentData(logs, mimeType: "application/zip", fileName: "Logs.zip")
-                didRetrieveLog = true
-                JSONFileManager.shared.deleteZip()
-            }
-
-            let subject = "Ithaca Transit Feedback v\(Constants.App.version)"
-            let body = createMessageBody(didRetrieveLog: didRetrieveLog)
-
-            mailComposerVC.setToRecipients([emailAddress])
-            mailComposerVC.setSubject(subject)
-            mailComposerVC.setMessageBody(body, isHTML: true)
-
-            present(mailComposerVC, animated: true)
-
-        } else {
-
-            let title = Constants.Alerts.EmailFailure.title
-            let message = Constants.Alerts.EmailFailure.message
-            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: Constants.Alerts.EmailFailure.emailSettings, style: .default, handler: { (_) in
-                let path = "App-Prefs:"
-                if let url = URL(string: path), UIApplication.shared.canOpenURL(url) {
-                    self.open(path, inApp: false)
-                } else {
-                    self.open(UIApplication.openSettingsURLString)
-                }
-                let payload = FeedbackErrorPayload(description: "Opened Email Settings")
-                Analytics.shared.log(payload)
-            }))
-            alertController.addAction(UIAlertAction(title: Constants.Alerts.EmailFailure.copyEmail, style: .default, handler: { (_) in
-                UIPasteboard.general.string = Constants.App.contactEmailAddress
-                let payload = FeedbackErrorPayload(description: "Copy Address to Clipboard")
-                Analytics.shared.log(payload)
-            }))
-            alertController.addAction(UIAlertAction(title: Constants.Alerts.EmailFailure.cancel, style: .default, handler: { (_) in
-                let payload = FeedbackErrorPayload(description: "Cancelled")
-                Analytics.shared.log(payload)
-            }))
-            present(alertController, animated: true)
-
-        }
-
     }
 
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Swift.Error?) {
@@ -322,7 +267,7 @@ class InformationViewController: UIViewController, UITableViewDataSource, UITabl
         UIView.animate(withDuration: duration, delay: delay, usingSpringWithDamping: damping,
                        initialSpringVelocity: velocity, options: options, animations: {
 
-            self.tcatImage.frame.origin.x += constant
+                        self.tcatImage.frame.origin.x += constant
 
         }, completion: { (_) in
 
@@ -331,7 +276,7 @@ class InformationViewController: UIViewController, UITableViewDataSource, UITabl
             UIView.animate(withDuration: duration, delay: delay, usingSpringWithDamping: damping,
                            initialSpringVelocity: velocity, options: options, animations: {
 
-                self.tcatImage.frame.origin.x += constant
+                            self.tcatImage.frame.origin.x += constant
 
             }, completion: { (_) in
 
@@ -350,4 +295,107 @@ class InformationViewController: UIViewController, UITableViewDataSource, UITabl
 
     }
 
+}
+
+// MARK: Table View Data Source
+extension InformationViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return content.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return content[section].count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // Initalize and format cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.Cells.informationCellIdentifier, for: indexPath)
+        cell.selectionStyle = .none
+        cell.backgroundColor = Colors.white
+        cell.textLabel?.textColor = Colors.primaryText
+        cell.textLabel?.font = .getFont(.regular, size: 14)
+        cell.textLabel?.textAlignment = .center
+
+        // Set cell content
+        cell.textLabel?.text = content[indexPath.section][indexPath.row].name
+
+        // Set custom formatting based on cell
+        switch (indexPath.section, indexPath.row) {
+
+        case (1, 1): // Send Feedback
+            cell.textLabel?.textColor = Colors.tcatBlue
+        default:
+            break
+
+        }
+
+        return cell
+
+    }
+}
+
+// MARK: Table View Delegate
+extension InformationViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selector = content[indexPath.section][indexPath.row].action
+        performSelector(onMainThread: selector, with: nil, waitUntilDone: false)
+    }
+}
+
+extension InformationViewController: MFMailComposeViewControllerDelegate {
+    @objc func sendFeedback() {
+
+        let emailAddress = Constants.App.contactEmailAddress
+
+        if MFMailComposeViewController.canSendMail() {
+
+            let mailComposerVC = MFMailComposeViewController()
+            mailComposerVC.mailComposeDelegate = self
+            var didRetrieveLog = false
+
+            if let logs = retrieveLogs() {
+                mailComposerVC.addAttachmentData(logs, mimeType: "application/zip", fileName: "Logs.zip")
+                didRetrieveLog = true
+                JSONFileManager.shared.deleteZip()
+            }
+
+            let subject = "Ithaca Transit Feedback v\(Constants.App.version)"
+            let body = createMessageBody(didRetrieveLog: didRetrieveLog)
+
+            mailComposerVC.setToRecipients([emailAddress])
+            mailComposerVC.setSubject(subject)
+            mailComposerVC.setMessageBody(body, isHTML: true)
+
+            present(mailComposerVC, animated: true)
+
+        } else {
+
+            let title = Constants.Alerts.EmailFailure.title
+            let message = Constants.Alerts.EmailFailure.message
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: Constants.Alerts.EmailFailure.emailSettings, style: .default, handler: { (_) in
+                let path = "App-Prefs:"
+                if let url = URL(string: path), UIApplication.shared.canOpenURL(url) {
+                    self.open(path, inApp: false)
+                } else {
+                    self.open(UIApplication.openSettingsURLString)
+                }
+                let payload = FeedbackErrorPayload(description: "Opened Email Settings")
+                Analytics.shared.log(payload)
+            }))
+            alertController.addAction(UIAlertAction(title: Constants.Alerts.EmailFailure.copyEmail, style: .default, handler: { (_) in
+                UIPasteboard.general.string = Constants.App.contactEmailAddress
+                let payload = FeedbackErrorPayload(description: "Copy Address to Clipboard")
+                Analytics.shared.log(payload)
+            }))
+            alertController.addAction(UIAlertAction(title: Constants.Alerts.EmailFailure.cancel, style: .default, handler: { (_) in
+                let payload = FeedbackErrorPayload(description: "Cancelled")
+                Analytics.shared.log(payload)
+            }))
+            present(alertController, animated: true)
+
+        }
+
+    }
 }
