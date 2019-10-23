9d80af2d8a66d4416452221a7f81ad2d93e16453
lucyxubroad <llx2@cornell.edu>
2019-10-23 13:57:28 -0400

    Remove spaces

--- TCAT/Views/SearchBarView.swift
+++ TCAT/Views/SearchBarView.swift
@@ -41,5 +41,4 @@ class SearchBarView: UIView, UISearchControllerDelegate {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
