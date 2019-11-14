7ce4024efbcd48ca307ca6dfa5cec9985ddf5deb
Daniel Vebman <daniel.vebman@gmail.com>
2019-11-14 09:11:57 -0500

    [MTSA][Refactor reachability][Part 1/2] Add ReachabilityManager (#348)
    * Create ReachabilityManager
    
    * Update closure removal and add retain test
    
    * Small fixes

---
+++ TCAT/Network/ReachabilityManager.swift
@@ -0,0 +1,57 @@
+//
+//  ReachabilityManager.swift
+//  TCAT
+//
+//  Created by Daniel Vebman on 11/6/19.
+//  Copyright © 2019 cuappdev. All rights reserved.
+//
+
+import Foundation
+
+class ReachabilityManager: NSObject {
+    
+    static let shared: ReachabilityManager = ReachabilityManager()
+    
+    private let reachability = Reachability()
+    private var listeners: [Pair] = []
+    
+    typealias Listener = AnyObject
+    typealias Closure = (Reachability.Connection) -> Void
+    
+    private struct Pair {
+        weak var listener: Listener?
+        var closure: Closure
+    }
+    
+    private override init() {
+        super.init()
+        
+        do {
+            try reachability?.startNotifier()
+        } catch {
+            print("[ReachabilityManager] init: Could not start reachability notifier.")
+        }
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(reachabilityChanged(_:)),
+            name: .reachabilityChanged,
+            object: reachability
+        )
+    }
+
+    /// Adds a listener to reachability updates.
+    /// Reminder: Be sure to begin the closure with `[weak self]`.
+    func addListener(_ listener: Listener, _ closure: @escaping Closure) {
+        listeners.append(Pair(listener: listener, closure: closure))
+    }
+    
+    @objc func reachabilityChanged(_ notification: Notification) {
+        guard let reachability = reachability else { return }
+        listeners = listeners.filter { pair -> Bool in
+            pair.closure(reachability.connection) // call the closures
+            return pair.listener != nil // remove closures for deinitialized listeners
+        }
+    }
+    
+}
