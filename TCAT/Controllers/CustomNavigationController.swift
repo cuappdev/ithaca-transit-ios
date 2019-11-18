f8f0a5954f16a1ce61927cb795d60954bb6a984c
= <daniel.vebman@gmail.com>
2019-11-18 15:53:38 -0500

    Return navigationcontroller reachability banner

--- TCAT/Controllers/CustomNavigationController.swift
+++ TCAT/Controllers/CustomNavigationController.swift
@@ -10,12 +10,12 @@ import NotificationBannerSwift
 import UIKit
 
 class CustomNavigationController: UINavigationController, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
-
+    
     /// Attributed string details for the back button text of a navigation controller
     static let buttonTitleTextAttributes: [NSAttributedString.Key: Any] = [
         .font: UIFont.getFont(.regular, size: 14)
     ]
-
+    
     private let additionalWidth: CGFloat = 30
     private let additionalHeight: CGFloat = 100
     /// Attributed string details for the title text of a navigation controller
@@ -23,32 +23,45 @@ class CustomNavigationController: UINavigationController, UINavigationController
         .font: UIFont.getFont(.regular, size: 18),
         .foregroundColor: Colors.black
     ]
-
+    
     private let banner = StatusBarNotificationBanner(title: Constants.Banner.noInternetConnection, style: .danger)
     private var screenshotObserver: NSObjectProtocol?
-
+    
     override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
         view.backgroundColor = Colors.white
         customizeAppearance()
+        
+        ReachabilityManager.shared.addListener(self) { [weak self] connection in
+            guard let self = self else { return }
+            switch connection {
+            case .wifi, .cellular:
+                self.banner.dismiss()
+            case .none:
+                self.banner.show(queuePosition: .front, on: self)
+                self.banner.autoDismiss = false
+                self.banner.isUserInteractionEnabled = false
+            }
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
     }
-
+    
     open override var childForStatusBarStyle: UIViewController? {
         return visibleViewController
     }
-
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return banner.isDisplaying ? .lightContent : .default
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if responds(to: #selector(getter: interactivePopGestureRecognizer)) {
             interactivePopGestureRecognizer?.delegate = self
             delegate = self
         }
-
+        
         // Add screenshot listener, log view controller name
         let notifName = UIApplication.userDidTakeScreenshotNotification
         screenshotObserver = NotificationCenter.default.addObserver(forName: notifName, object: nil, queue: .main) { _ in
@@ -57,15 +70,15 @@ class CustomNavigationController: UINavigationController, UINavigationController
             Analytics.shared.log(payload)
         }
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
+        
         if let screenshotObserver = screenshotObserver {
             NotificationCenter.default.removeObserver(screenshotObserver)
         }
     }
-
+    
     private func customizeAppearance() {
         navigationBar.backgroundColor = Colors.white
         navigationBar.barTintColor = Colors.white
@@ -75,13 +88,13 @@ class CustomNavigationController: UINavigationController, UINavigationController
             CustomNavigationController.buttonTitleTextAttributes, for: .normal
         )
     }
-
+    
     /// Return an instance of custom back button
     private func customBackButton() -> UIBarButtonItem {
         let backButton = UIButton()
         backButton.setImage(#imageLiteral(resourceName: "back"), for: .normal)
         backButton.tintColor = Colors.primaryText
-
+        
         let attributes: [NSAttributedString.Key: Any] = [
             .font: UIFont.getFont(.regular, size: 14.0),
             .foregroundColor: Colors.primaryText,
@@ -90,33 +103,33 @@ class CustomNavigationController: UINavigationController, UINavigationController
         let attributedString = NSMutableAttributedString(string: "  " + Constants.Buttons.back, attributes: attributes)
         backButton.setAttributedTitle(attributedString, for: .normal)
         backButton.sizeToFit()
-
+        
         // Expand frame to create bigger touch area
         backButton.frame = CGRect(x: backButton.frame.minX, y: backButton.frame.minY, width: backButton.frame.width + additionalWidth, height: backButton.frame.height + additionalHeight)
         backButton.contentEdgeInsets = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: additionalWidth)
-
+        
         backButton.addTarget(self, action: #selector(backAction), for: .touchUpInside)
-
+        
         return UIBarButtonItem(customView: backButton)
     }
-
+    
     /// Move back one view controller in navigationController stack
     @objc private func backAction() {
         _ = popViewController(animated: true)
     }
-
+    
     // MARK: - UINavigationController Functions
-
+    
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         if responds(to: #selector(getter: interactivePopGestureRecognizer)) {
             interactivePopGestureRecognizer?.isEnabled = false
         }
-
+        
         super.pushViewController(viewController, animated: animated)
-
+        
         if viewControllers.count > 1 {
             navigationBar.titleTextAttributes = titleTextAttributes
-
+            
             // Add back button for non-modal non-peeked screens
             if !viewController.isModal {
                 viewController.navigationItem.hidesBackButton = true
@@ -124,60 +137,59 @@ class CustomNavigationController: UINavigationController, UINavigationController
             }
         }
     }
-
+    
     override func popViewController(animated: Bool) -> UIViewController? {
-
         let viewController = super.popViewController(animated: animated)
         if let homeMapVC = viewControllers.last as? HomeMapViewController {
             homeMapVC.navigationItem.leftBarButtonItem = nil
         }
         return viewController
     }
-
+    
     override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
         super.present(viewControllerToPresent, animated: flag, completion: completion)
         banner.dismiss()
     }
-
+    
     // MARK: - UINavigationControllerDelegate Functions
-
+    
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         setNavigationBarHidden(viewController is HomeMapViewController, animated: animated)
     }
-
+    
     func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
         interactivePopGestureRecognizer?.isEnabled = (responds(to: #selector(getter: interactivePopGestureRecognizer)) && viewControllers.count > 1)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
 }
 
 class OnboardingNavigationController: UINavigationController {
-
+    
     override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
         navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationBar.shadowImage = UIImage()
         navigationBar.isTranslucent = true
     }
-
+    
     open override var childForStatusBarStyle: UIViewController? {
         return visibleViewController
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
+    
     // Do NOT remove this initializer; OnboardingNavigationController will crash without it.
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
-
+    
 }
