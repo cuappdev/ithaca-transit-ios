ca57123efe1fff8946d53a973b26827418ce8880
Vin Bui <75594943+vinnie4k@users.noreply.github.com>
2024-02-24 15:43:00 -0500

    Merge `release` to `master` (#372)
    * Added post clone script
    
    * Update ci_post_clone.sh to correct environment variable name
    
    * Changed version and build
    
    * Change script to add files correctly
    
    * Change script to add files correctly attempt 2
    
    * Change script to add files correctly attempt 3
    
    * Change script to add files correctly attempt 4
    
    * Change script to add files correctly attempt 5
    
    * Change script to add files correctly attempt 6
    
    * Change script to add files correctly attempt 7
    
    ---------
    
    Co-authored-by: Jayson Hahn <46629787+JaysonHahn@users.noreply.github.com>

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  TCAT
+#
+#  Created by Jayson  Hahn on 2/23/24.
+#  Copyright © 2024 cuappdev. All rights reserved.
+
+echo "Installing Pods"
+
+# Install CocoaPods using Homebrew.
+brew install cocoapods
+
+# Install dependencies you manage with CocoaPods.
+pod install
+
+echo "Downloading Secrets"
+
+# Install wget using Homebrew.
+brew install wget
+
+# Create directories if they don't exist.
+cd ../TCAT
+mkdir Firebase
+cd Firebase
+mkdir Dev
+mkdir Prod
+
+# Change directory to ci_scripts
+cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts
+
+# Download files
+wget -O ../TCAT/Firebase/Prod/GoogleService-Info.plist "$DEV_GOOGLE_SERVICE_PLIST"
+wget -O ../TCAT/Firebase/Dev/GoogleService-Info.plist "$PROD_GOOGLE_SERVICE_PLIST"
+wget -O ../TCAT/Supporting\ Files/Keys.plist "$KEYS_PLIST"
+
