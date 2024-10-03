b64690f1305610e6bd6d26e764e71991f32b5bb5
cindy-x-liang <67083541+cindy-x-liang@users.noreply.github.com>
2024-10-03 10:08:51 -0400

    addressing richie's comments

--- TCAT/Base/AppDelegate.swift
+++ TCAT/Base/AppDelegate.swift
@@ -100,18 +100,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
           options: authOptions,
           completionHandler: { _, _ in }
         )
-
         application.registerForRemoteNotifications()
-        
-        
         Messaging.messaging().delegate = self
         
-        
-        
-
-        
-      
-
         return true
     }
 
@@ -122,15 +113,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
         Messaging.messaging().token { token, error in
-          if let error = error {
-            print("Error fetching FCM registration token: \(error)")
-          } else if let token = token {
-            print("FCM registration token: \(token)")
-            
-          }
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+                
+            }
         }
-           
-       }
+        
+    }
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("application didFailToRegisterForRemoteNotificationsWithError")
     }
@@ -245,24 +236,18 @@ extension UIWindow {
 }
 
 extension AppDelegate {
-
-//MessagingDelegate
-//    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-//        self.firebaseToken = fcmToken!
-//        print("Firebase token: \(fcmToken)")
-//    }
     
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("Firebase registration token: \(String(describing: fcmToken))")
-
-      let dataDict: [String: String] = ["token": fcmToken ?? ""]
-      NotificationCenter.default.post(
-        name: Notification.Name("FCMToken"),
-        object: nil,
-        userInfo: dataDict
-      )
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
 
     //UNUserNotificationCenterDelegate
