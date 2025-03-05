f5bebc41884f36f2c0e404753b0d476f1bbcf4a1
ako28 <futureguy5760@gmail.com>
2025-03-05 17:28:04 -0500

    new branch

---
+++ TCAT/UI/InformationMainView/Cells/NewInformationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewInformationTableViewCell.swift
+//  TCAT
+//
+//  Created by Asen Ou on 3/4/25.
+//  Copyright © 2025 Cornell AppDev. All rights reserved.
+//
+
+import UIKit
+
+class NewInformationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
